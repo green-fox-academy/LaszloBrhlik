7b235e7d2bfadb0412b3485d7a03588388de4d77
Laszlo Brhlik <brhlik.laszlo@gmail.com>
2018-02-16 11:41:10 +0100

    Exercise-17-added

---
+++ Week-01/day-04/src/Exercise16.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Exercise16 {
+
+        public static void main(String[] args) {
+            // Write a program that asks for two integers
+            // The first represents the number of chickens the farmer has
+            // The second represents the number of pigs owned by the farmer
+            // It should print how many legs all the animals have
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Add please the number of chickens:");
+
+            int chickenNr = scanner.nextInt();
+
+            System.out.println("Add please the number of pigs:");
+
+            int pigNr = scanner.nextInt();
+
+            int totalLegs = chickenNr * 2 + pigNr * 4;
+
+            System.out.println("Total legs are: " + totalLegs);
+
+
+        }
+    }
+
---
+++ Week-01/day-04/src/Exercise17.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Exercise17 {
+
+    public static void main(String[] args) {
+
+        // Write a program that asks for 5 integers in a row,
+        // then it should print the sum and the average of these numbers like:
+        //
+        // Sum: 22, Average: 4.4
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please add 5 numbers:");
+
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+        int number3 = scanner.nextInt();
+        int number4 = scanner.nextInt();
+        int number5 = scanner.nextInt();
+        int sum5 = number1+number2+number3+number4+number5;
+        int average5 = (number1+number2+number3+number4+number5)/5;
+
+        System.out.println("Sum: " + sum5 + ", Average: " + average5);
+    }
+
+
+}
