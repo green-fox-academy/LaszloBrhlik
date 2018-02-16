ee0818a64011da64581fe4668873be92dc67ff83
Laszlo Brhlik <brhlik.laszlo@gmail.com>
2018-02-16 11:53:55 +0100

    Exercise-19-added

---
+++ Week-01/day-04/src/Exercise18.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Exercise18 {
+
+    public static void main(String[] args) {
+
+        // Write a program that reads a number from the standard input,
+        // Then prints "Odd" if the number is odd, or "Even" if it is even.
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please add a number:");
+        int number = scanner.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println("This is an \"even\" number");
+        } else {
+            System.out.println("This is an \"odd\" number");
+        }
+
+
+    }
+
+}
