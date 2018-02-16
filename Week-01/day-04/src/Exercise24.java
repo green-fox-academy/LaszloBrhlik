94fedc052ae5866e69c49ef1c4ffa41b9f246e8e
Laszlo Brhlik <brhlik.laszlo@gmail.com>
2018-02-16 12:42:31 +0100

    Exercise-24-added

---
+++ Week-01/day-04/src/Exercise24.java
@@ -0,0 +1,18 @@
+public class Exercise24 {
+
+    public static void main(String[] args) {
+
+        // Create a program that prints all the even numbers between 0 and 500
+
+        for (int i=1; i<501; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+
+//I considered 2 as first even number and 500 as the last one...
+
+
+
+    }
+}
