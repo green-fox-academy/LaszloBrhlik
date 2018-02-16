601cae018386163cbd9c11c7cf550bddf572f37d
Laszlo Brhlik <brhlik.laszlo@gmail.com>
2018-02-16 17:12:53 +0100

    Exercise-29-added

--- Week-01/day-04/src/Exercise29.java
+++ Week-01/day-04/src/Exercise29.java
@@ -23,14 +23,14 @@ public class Exercise29 {
 
         int number = scanner.nextInt();
 
-        for (int i = (number+1), k = 0; i > 0 && k < (number+1); i--, k++) {
-            for (int j = 0; j < i; j++) {
+        for (int i = (number+1), j = 0; i > 0 && j < (number+1); i--, j++) {
+            for (int k = 0; k < i; k++) {
                 System.out.print(" ");
             }
-            for (int j = 0; j < k; j++) {
+            for (int k = 0; k < j; k++) {
                 System.out.print("*");
             }
-            for (int j = 1; j < k; j++) {
+            for (int k = 1; k < j; k++) {
                 System.out.print("*");
             }
             System.out.println();
