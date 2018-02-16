e77e1f7c1bed169327e2068fa4561b976606e223
Laszlo Brhlik <brhlik.laszlo@gmail.com>
2018-02-16 11:06:24 +0100

    Exercise-14-added

---
+++ Week-01/day-04/src/Exercise14.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Exercise14 {
+
+    public static void main(String[] args) {
+        // Modify this program to greet user instead of the World!
+        // The program should ask for the name of the user
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please add your username!");
+
+        String userInput1 = scanner.nextLine();
+
+        System.out.println("Greetings user: " + userInput1);
+
+
+
+
+    }
+}
