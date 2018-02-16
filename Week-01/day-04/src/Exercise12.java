48859f5bda0ae00bee3ddf250b215a413fe19f5f
Laszlo Brhlik <brhlik.laszlo@gmail.com>
2018-02-16 10:51:21 +0100

    Exercise-12-added

---
+++ Week-01/day-04/src/Exercise12.java
@@ -0,0 +1,24 @@
+public class Exercise12 {
+
+    public static void main(String[] args) {
+// Write a program that stores 3 sides of a cuboid as variables (doubles)
+// The program should write the surface area and volume of the cuboid like:
+//
+// Surface Area: 600
+// Volume: 1000
+
+        double length = 5;
+        double depth = 6;
+        double height = 7;
+
+        double surface = length * depth;
+        double volume = length * depth * height;
+
+        System.out.println("Area of the cuboid is: " + surface);
+        System.out.println("Volume of the cuboid is: " + volume);
+
+
+    }
+
+
+}
