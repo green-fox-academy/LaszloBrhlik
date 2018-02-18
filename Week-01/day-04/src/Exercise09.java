9ac9f1ce29535ec940ba1c00d3458568d876a71e
Laszlo Brhlik <brhlik.laszlo@gmail.com>
2018-02-18 12:07:15 +0100

    consistent code

--- Week-01/day-04/src/Exercise09.java
+++ Week-01/day-04/src/Exercise09.java
@@ -3,7 +3,6 @@ public class Exercise09 {
     public static void main(String[] args) {
         double massInKg = 81.2;
         double heightInM = 1.78;
-
         // Print the Body mass index (BMI) based on these values
 
         double heightInM2 = heightInM * heightInM;
