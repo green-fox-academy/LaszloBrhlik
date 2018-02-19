b299f9d67138788505eacc172b0f0a696d424477
Laszlo Brhlik <brhlik.laszlo@gmail.com>
2018-02-19 17:22:26 +0100

    Exercise01_Doubling_added

--- week-02/day-01/src/Exercise01_Doubling.java
+++ week-02/day-01/src/Exercise01_Doubling.java
@@ -1,5 +1,15 @@
 public class Exercise01_Doubling {
+
     public static void main(String[] args) {
-        //in progress..
+        // - Create an integer variable named `ak` and assign the value `123` to it
+        // - Create a function called `doubling` that doubles it's input parameter and returns with an integer
+        // - Print the result of `doubling(ak)`
+        int ak = 123;
+        System.out.println(doubling(ak));
+
+    }
+    public static int doubling(int a) {
+        a = 2 * a;
+        return a;
     }
 }
