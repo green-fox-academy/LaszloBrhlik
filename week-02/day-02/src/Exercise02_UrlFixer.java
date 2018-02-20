4d83ecf7b805bcf3e3d6f20e8e8153bef8b4fea0
Laszlo Brhlik <brhlik.laszlo@gmail.com>
2018-02-20 12:24:47 +0100

    w02_d02_Excersise03_TakesLonger_added

--- week-02/day-02/src/Exercise02_UrlFixer.java
+++ week-02/day-02/src/Exercise02_UrlFixer.java
@@ -3,12 +3,15 @@ public class Exercise02_UrlFixer {
         String url = "https//www.reddit.com/r/nevertellmethebots";
 
         // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
-        // Also, the URL is missing a crutial component, find out what it is and insert it too!
+        // Also, the URL is missing a crucial component, find out what it is and insert it too!
         // Try to solve it more than once using different String functions!
 
-        url = url.replace("bots", "odds");
-        url = url.replace("https", "https:");
+        String url1 = url.replace("bots", "odds");
+        String url2 = url1.replace("https", "https:");
 
-        System.out.println(url);
+        String url3 = "https:" + url.substring(5,38) + "odds";
+
+        System.out.println(url3);
+        System.out.println(url2);
     }
 }
