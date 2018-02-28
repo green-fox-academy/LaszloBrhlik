56554a027b5c078ebfae3fa4c71543faf302b6eb
Laszlo Brhlik <brhlik.laszlo@gmail.com>
2018-02-28 11:39:54 +0100

    feat(PostIt): completed

---
+++ week-03/day-03/src/Exercise01_PostIt.java
@@ -0,0 +1,11 @@
+public class Exercise01_PostIt {
+  public String backgroundColor;
+  public String text;
+  public String textColor;
+
+  public Exercise01_PostIt(String backgroundColor, String text, String textColor) {
+    this.backgroundColor = backgroundColor;
+    this.text = text;
+    this.textColor = textColor;
+  }
+}
---
+++ week-03/day-03/src/Exercise01_PostItMain.java
@@ -0,0 +1,7 @@
+public class Exercise01_PostItMain {
+  public static void main(String[] args) {
+    Exercise01_PostIt object1 = new Exercise01_PostIt("orange", "Idea1", "blue");
+    Exercise01_PostIt object2 = new Exercise01_PostIt("pink", "Awesome", "black");
+    Exercise01_PostIt object3 = new Exercise01_PostIt("yellow", "Superb", "green");
+  }
+}
