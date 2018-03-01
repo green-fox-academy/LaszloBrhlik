80ae78c2b776bb848e9448660d2d96b7bc46b702
Laszlo Brhlik <brhlik.laszlo@gmail.com>
2018-03-01 10:29:56 +0100

    feat(NumberAdder): comleted

--- week-03/day-04/src/NumberAdder.java
+++ week-03/day-04/src/NumberAdder.java
@@ -1,7 +1,15 @@
 public class NumberAdder {
   public static void main(String[] args) {
     // Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
-
-    
+    System.out.println(numberAdder(4));
+  }
+  public static int numberAdder(int n) {
+    int result;
+    if (n == 1) {
+      return 1;
+    } else {
+      result = n + numberAdder(n-1);
+    }
+    return result;
   }
 }
