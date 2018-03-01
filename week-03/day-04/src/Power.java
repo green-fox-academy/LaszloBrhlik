15665105884216968c60d6aac6d555908b5e61b1
Laszlo Brhlik <brhlik.laszlo@gmail.com>
2018-03-01 11:02:07 +0100

    feat(Power): completed

---
+++ week-03/day-04/src/Power.java
@@ -0,0 +1,18 @@
+public class Power {
+  public static void main(String[] args) {
+    // Given base and n that are both 1 or more, compute recursively (no loops)
+    // the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+
+    System.out.println(power(4,4));
+
+  }
+  public static int power(int base, int n) {
+    if (base ==0) {
+      return 0;
+    } else if (n==1) {
+      return base;
+    } else {
+      return base * power(base, n-1);
+    }
+  }
+}
---
+++ week-03/day-04/src/SumDigit.java
@@ -0,0 +1,21 @@
+public class SumDigit {
+  public static void main(String[] args) {
+    // Given a non-negative int n, return the sum of its digits recursively (no loops).
+    // Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while
+    // divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+
+    System.out.println(sumDigit(155));
+
+  }
+  public static int sumDigit(int n) {
+    int result = 0;
+    if (n == 0) {
+      return 0;
+    } else {
+      result = n%10 + sumDigit(n/10);
+    }
+    return result;
+  }
+}
+
+
