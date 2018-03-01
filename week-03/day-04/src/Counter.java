ea57ac6ceffbf89f54075e8198fc1f5827526904
Laszlo Brhlik <brhlik.laszlo@gmail.com>
2018-03-01 10:19:33 +0100

    feat(Counter): comleted

---
+++ week-03/day-04/src/Counter.java
@@ -0,0 +1,18 @@
+public class Counter {
+  public static void main(String[] args) {
+    // Write a recursive function that takes one parameter: n and counts down from n.
+    counter(10);
+
+  }
+
+  public static void counter(int n) {
+    if (n == 0) {
+      System.out.println(0);
+    } else {
+      System.out.println(n);
+      counter(n - 1);
+    }
+  }
+}
+
+
