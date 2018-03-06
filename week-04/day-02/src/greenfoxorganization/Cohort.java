d9362cd47e8ee1f910daffe66498d77875bc4f66
Laszlo Brhlik <brhlik.laszlo@gmail.com>
2018-03-06 12:43:19 +0100

    feat(greenfoxorganization): Cohort added

---
+++ week-04/day-02/src/greenfoxorganization/Cohort.java
@@ -0,0 +1,31 @@
+package greenfoxorganization;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Cohort {
+
+  String name;
+  List<Student> students;
+  List<Mentor> mentors;
+
+  public Cohort(String name) {
+    this.name = name;
+    this.students = Collections.emptyList();
+    this.mentors = Collections.emptyList();
+  }
+
+  public void addStudent(Student student) {
+    this.students.add(student);
+  }
+
+  public void addMentor(Mentor mentor) {
+    this.mentors.add(mentor);
+  }
+
+  public void info() {
+    System.out.println("The " + name + " cohort has " + students.size() + " students and " +
+    mentors.size() + " mentors.");
+
+  }
+}
