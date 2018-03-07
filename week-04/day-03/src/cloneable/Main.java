e6afeaa233768e455a5b902d9f466d239bb36a01
Laszlo Brhlik <brhlik.laszlo@gmail.com>
2018-03-07 12:33:33 +0100

    feat(cloneable): completed

--- week-04/day-03/src/cloneable/Main.java
+++ week-04/day-03/src/cloneable/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
   public static void main(String[] args) throws CloneNotSupportedException {
 
-    Student henry = new Student();
+    Student henry = new Student("Henry", 30, "Male", "Google");
     Student john = (Student) henry.clone();
-
+    john.introduce();
 
   }
 }
