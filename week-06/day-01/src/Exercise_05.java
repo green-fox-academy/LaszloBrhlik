8985ccbfec040c9595d60ef73bf282e1837e9b1f
Laszlo Brhlik <brhlik.laszlo@gmail.com>
2018-03-26 13:10:07 +0200

    feat(Exercise_05) completed

--- week-06/day-01/src/Exercise_05.java
+++ week-06/day-01/src/Exercise_05.java
@@ -1,11 +1,13 @@
+import java.util.Arrays;
+
 public class Exercise_05 {
   public static void main(String[] args) {
     //Write a Stream Expression to find the uppercase characters in a string!
     String example = "AaBbCcDd";
 
     example.chars()
+            .filter(Character::isUpperCase)
             .mapToObj(c -> (char) c)
-            .filter(c -> c.isUpperCase(c))
             .forEach(System.out::println);
   }
 }
