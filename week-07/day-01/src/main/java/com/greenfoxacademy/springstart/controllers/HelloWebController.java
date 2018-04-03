2834a5a1302ba25279a729265f94c2f55ff3693b
Laszlo Brhlik <brhlik.laszlo@gmail.com>
2018-04-03 17:25:41 +0200

    feat(HelloWebController) say hello to all world

--- week-07/day-01/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-07/day-01/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -25,4 +25,17 @@ public class HelloWebController {
     return "greeting2";
     // http://localhost:8080/web/greeting2?name=Laca
   }
+
+  @RequestMapping("/web/greeting3")
+  public String greeting3(Model model) {
+    String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag",
+            "Goede dag", "Hallo", "Hello", "Saluton", "Hei", "Bonjour", "Guten Tag", "Gia'sou", "Aloha",
+            "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai",
+            "Qanuipit", "Dia dhuit", "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao",
+            "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+            "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+    model.addAttribute("hellos", hellos);
+    return "greeting3";
+    // http://localhost:8080/web/greeting3
+  }
 }
