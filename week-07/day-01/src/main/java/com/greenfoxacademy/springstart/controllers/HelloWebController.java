35f741b8d5e24f0a2b7593524bad39f6cbe532c5
Laszlo Brhlik <brhlik.laszlo@gmail.com>
2018-04-03 14:39:52 +0200

    feat(HelloWebController) web greet counter app

--- week-07/day-01/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-07/day-01/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -3,13 +3,26 @@ package com.greenfoxacademy.springstart.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class HelloWebController {
+  AtomicLong atomicLong = new AtomicLong();
 
-  @RequestMapping("/web/greeting")
-  public String greeting(Model model) {
-    model.addAttribute("name", " World");
-    return "greeting";
+  @RequestMapping("/web/greeting1")
+  public String greeting1(Model model) {
+    model.addAttribute("name", "World");
+    return "greeting1";
+    // @ResponseBody would return method's body not related .html
+  }
+
+  @RequestMapping("/web/greeting2")
+  public String greeting2(Model model, @RequestParam("name") String content) {
+    model.addAttribute("name", content);
+    model.addAttribute("id", atomicLong.getAndIncrement());
+    return "greeting2";
+    // http://localhost:8080/web/greeting2?name=Laca
   }
 }
