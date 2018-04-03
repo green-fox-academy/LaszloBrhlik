37752167af4813216c55c37e912cf18d26f5d99d
Laszlo Brhlik <brhlik.laszlo@gmail.com>
2018-04-03 13:42:23 +0200

    feat(HelloRESTController) AtomicLong extended

--- week-07/day-01/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day-01/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -4,8 +4,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
-public class HelloRESTController {
+public class HelloRESTController extends AtomicLong{
 
   @RequestMapping("/greeting1")
   public Greeting greeting1() {
@@ -16,5 +18,11 @@ public class HelloRESTController {
   public Greeting greeting2(@RequestParam("id") long id, @RequestParam("name") String content) {
     return new Greeting(id, content);
     // http://localhost:8080/greeting2?id=1&name=Laca
+  }
+
+  @RequestMapping("/greeting3")
+  public Greeting greeting3(@RequestParam("name") String content) {
+    return new Greeting(getAndIncrement(), content);
+    // http://localhost:8080/greeting3?name=Laca
   }
 }
