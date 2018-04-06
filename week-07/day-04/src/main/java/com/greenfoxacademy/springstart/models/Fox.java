96acf7acb43347a00563aa3110d761eea2cbab8a
Laszlo Brhlik <brhlik.laszlo@gmail.com>
2018-04-06 12:27:22 +0200

    feat(Fox, FoxDB) models and tempDB packages created with related classes

---
+++ week-07/day-04/src/main/java/com/greenfoxacademy/springstart/models/Fox.java
@@ -0,0 +1,10 @@
+package com.greenfoxacademy.springstart.models;
+
+public class Fox {
+
+  String name;
+
+  public Fox(String name) {
+    this.name = name;
+  }
+}
---
+++ week-07/day-04/src/main/java/com/greenfoxacademy/springstart/tempDB/FoxDB.java
@@ -0,0 +1,15 @@
+package com.greenfoxacademy.springstart.tempDB;
+
+import com.greenfoxacademy.springstart.models.Fox;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class FoxDB {
+
+  List<Fox> foxes = new ArrayList<>();
+
+  }
+
