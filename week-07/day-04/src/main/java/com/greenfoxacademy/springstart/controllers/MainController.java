3e1d1293f9836b78b98d2e000ade7f6a5a9706fa
Laszlo Brhlik <brhlik.laszlo@gmail.com>
2018-04-06 12:20:49 +0200

    feat(MainController) getmapping for login.html

--- week-07/day-04/src/main/java/com/greenfoxacademy/springstart/controllers/MainController.java
+++ week-07/day-04/src/main/java/com/greenfoxacademy/springstart/controllers/MainController.java
@@ -2,13 +2,25 @@ package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
 
 
-  @GetMapping("/")
-  public String index() {
+  @GetMapping("/index")
+  public String indexGet() {
     return "index";
   }
+
+  @GetMapping("/login")
+  public String loginGet() {
+    return "login";
+  }
+
+  @PostMapping("/login")
+  public String loginPost() {
+    return "login";
+  }
+
 }
