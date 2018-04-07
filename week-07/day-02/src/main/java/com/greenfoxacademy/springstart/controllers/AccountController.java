1cdd116d80db6f82193f2184072f9f23a66fbadb
Laszlo Brhlik <brhlik.laszlo@gmail.com>
2018-04-07 12:48:52 +0200

    feat(LionKingDB): temporary database created to store accounts

--- week-07/day-02/src/main/java/com/greenfoxacademy/springstart/controllers/AccountController.java
+++ week-07/day-02/src/main/java/com/greenfoxacademy/springstart/controllers/AccountController.java
@@ -1,32 +1,31 @@
 package com.greenfoxacademy.springstart.controllers;
 
-import com.greenfoxacademy.springstart.models.BankAccount;
+import com.greenfoxacademy.springstart.tempDB.LionKingDB;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
-import java.util.List;
-
 @Controller
 public class AccountController {
-  BankAccount bankAccount1 = new BankAccount("Simba", 4000, "lion", true);
-  BankAccount bankAccount2 = new BankAccount("Timon", 3000, "ophuzi mongoose", false);
-  BankAccount bankAccount3 = new BankAccount("Pumba", 2000, "pig", false);
-  BankAccount bankAccount4 = new BankAccount("Mufasa", 2000, "lion", false);
-  BankAccount bankAccount5 = new BankAccount("Scar", 1000, "lion", false);
-  List<BankAccount> bankAccounts = Arrays.asList(bankAccount1, bankAccount2, bankAccount3, bankAccount4, bankAccount5);
 
-  @GetMapping("/showmultiple")
+  public LionKingDB lionKingDB;
+
+  @Autowired
+  public AccountController(LionKingDB lionKingDB) {
+    this.lionKingDB = lionKingDB;
+  }
+
+  @GetMapping("/web/accounts")
   public String show(Model model) {
-    model.addAttribute("bankAccounts", bankAccounts);
+    model.addAttribute("accounts", lionKingDB.accounts);
     return "accounts";
   }
 
-  @PostMapping("/showmultiple")
+  @PostMapping("/web/accounts")
   public String donate() {
-    bankAccounts.stream()
-            .forEach(bankAccount -> bankAccount.setBalance());
-    return "accounts";
+    lionKingDB.accounts.stream()
+            .forEach(account -> account.setBalance());
+    return "redirect:/web/accounts";
   }
 }
--- week-07/day-02/src/main/java/com/greenfoxacademy/springstart/tempDB/LionKingDB.java
+++ week-07/day-02/src/main/java/com/greenfoxacademy/springstart/tempDB/LionKingDB.java
@@ -1,4 +1,26 @@
 package com.greenfoxacademy.springstart.tempDB;
 
+import com.greenfoxacademy.springstart.models.BankAccount;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
 public class LionKingDB {
+
+  public List<BankAccount> accounts;
+
+  @Autowired
+  public LionKingDB() {
+    accounts = new ArrayList<>(Arrays.asList(
+            new BankAccount("Simba", 4000, "lion", true),
+            new BankAccount("Timon", 3000, "ophuzi mongoose", false),
+            new BankAccount("Pumba", 2000, "pig", false),
+            new BankAccount("Mufasa", 2000, "lion", false),
+            new BankAccount("Scar", 1000, "lion", false)
+    ));
+  }
 }
