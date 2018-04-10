a770c52d8b694905a3dc27ce1074c8115ae92da7
Laszlo Brhlik <brhlik.laszlo@gmail.com>
2018-04-10 12:16:55 +0200

    feat(H2projectApplication) commandlinerunner implemented, new todos added

--- week-08/day-02/h2project/src/main/java/com/greenfoxacademy/h2project/H2projectApplication.java
+++ week-08/day-02/h2project/src/main/java/com/greenfoxacademy/h2project/H2projectApplication.java
@@ -1,12 +1,26 @@
 package com.greenfoxacademy.h2project;
 
+import com.greenfoxacademy.h2project.models.Todo;
+import com.greenfoxacademy.h2project.repositories.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class H2projectApplication {
+public class H2projectApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(H2projectApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(H2projectApplication.class, args);
+  }
+
+  @Autowired
+  private TodoRepository todoRepository;
+
+  @Override
+  public void run(String... args) throws Exception {
+    todoRepository.save(new Todo("I have to learn Object Relational Mapping"));
+    todoRepository.save(new Todo("I have to relax a lot"));
+    todoRepository.save(new Todo("And eat"));
+  }
 }
--- week-08/day-02/h2project/src/main/java/com/greenfoxacademy/h2project/controllers/TodoController.java
+++ week-08/day-02/h2project/src/main/java/com/greenfoxacademy/h2project/controllers/TodoController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping(value = "/todo")
@@ -16,8 +15,8 @@ public class TodoController {
   private TodoRepository todoRepository;
 
   @GetMapping(value={"/", "list"})
-  @ResponseBody
   public String list (Model model) {
-    return "This is my first todo";
+    model.addAttribute("todos", todoRepository.findAll());
+    return "todoslist";
   }
 }
