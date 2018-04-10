160dcbef81d77fa0a83b98f8db3b6572d9633189
Laszlo Brhlik <brhlik.laszlo@gmail.com>
2018-04-10 11:29:10 +0200

    feat(TodoController): return string working fine

--- week-08/day-02/h2project/src/main/java/com/greenfoxacademy/h2project/models/Todo.java
+++ week-08/day-02/h2project/src/main/java/com/greenfoxacademy/h2project/models/Todo.java
@@ -1,4 +1,9 @@
 package com.greenfoxacademy.h2project.models;
 
 public class Todo {
+
+  private long id;
+  private String title;
+  private boolean urgent = false;
+  private boolean done = false;
 }
