46d0ec15b5a608e1c976462d2521fcad69cae023
Laszlo Brhlik <brhlik.laszlo@gmail.com>
2018-04-10 11:20:28 +0200

    first commit: h2project setup

---
+++ week-08/day-02/h2project/src/main/java/com/greenfoxacademy/h2project/H2projectApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.h2project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2projectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2projectApplication.class, args);
+	}
+}
---
+++ week-08/day-02/h2project/src/main/java/com/greenfoxacademy/h2project/models/Todo.java
@@ -0,0 +1,4 @@
+package com.greenfoxacademy.h2project.models;
+
+public class Todo {
+}
